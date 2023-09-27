b48c5fa756a021987479b336d6e4dc2be04747ed
gift56 <efeasiughu@gmail.com>
2023-09-27 12:36:29 -0400

    working on the main page

--- src/components/ProgramComponents/ProgramHeader.tsx
+++ src/components/ProgramComponents/ProgramHeader.tsx
@@ -1,5 +1,14 @@
 const ProgramHeader = () => {
-  return <nav aria-label="programheading" className="w-full flex items-start justify-between gap-8">ProgramHeader</nav>;
+  return (
+    <nav
+      aria-label="programheading"
+      className="w-full flex items-start justify-between gap-8"
+    >
+      <div className="flex items-start justify-start">
+        <h2 className="text-base font-semibold">London Internship Program</h2>
+      </div>
+    </nav>
+  );
 };
 
 export default ProgramHeader;
