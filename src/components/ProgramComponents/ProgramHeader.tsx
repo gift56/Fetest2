504fb02ff303c0f7fb0572932b1fd855efc3fd2a
gift56 <efeasiughu@gmail.com>
2023-09-27 12:37:19 -0400

    working on the main page

--- src/components/ProgramComponents/ProgramHeader.tsx
+++ src/components/ProgramComponents/ProgramHeader.tsx
@@ -5,7 +5,10 @@ const ProgramHeader = () => {
       className="w-full flex items-start justify-between gap-8"
     >
       <div className="flex items-start justify-start">
-        <h2 className="text-base font-semibold">London Internship Program</h2>
+        <h2 className="text-base font-semibold text-primary md:text-xl">
+          London Internship Program
+        </h2>
+        <span className="">London</span>
       </div>
     </nav>
   );
