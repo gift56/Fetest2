3b31b41270654722a22ef7bf6a08c0a05055e344
gift56 <efeasiughu@gmail.com>
2023-09-27 12:33:05 -0400

    working on the main page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -3,7 +3,7 @@ import MainLayout from "../layout/MainLayout";
 const ProgramViewPage = () => {
   return (
     <MainLayout title="FE OverView Task Test - Program Application View">
-      <div className="w-full h-screen flex flex-col items-start justify-start gap-6"></div>
+      <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6"></div>
     </MainLayout>
   );
 };
