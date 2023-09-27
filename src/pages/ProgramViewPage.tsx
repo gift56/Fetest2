8d5d07bbb28503fbb02e66a6e2d4083b715cbd9a
gift56 <efeasiughu@gmail.com>
2023-09-27 14:04:08 -0400

    done working on the main page header

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -6,6 +6,9 @@ const ProgramViewPage = () => {
     <MainLayout title="FE OverView Task Test - Program Application View">
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
         <ProgramHeader />
+        <div className="w-full flex items-start justify-start gap-6">
+          
+        </div>
       </div>
     </MainLayout>
   );
