b0e0d08dd6b81833ba1733cd612c3f0d42461805
gift56 <efeasiughu@gmail.com>
2023-09-27 15:51:22 -0400

    working on opportunity

--- src/pages/OpportunityViewPage.tsx
+++ src/pages/OpportunityViewPage.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "../layout/MainLayout";
+import { BiChevronDown } from "react-icons/bi";
 
 const OpportunityViewPage = () => {
   return (
@@ -8,6 +9,12 @@ const OpportunityViewPage = () => {
           <h2 className="text-base font-semibold text-primary md:text-xl">
             Opportunity Overview
           </h2>
+          <div className="flex items-center justify-end gap-6">
+            <span className="flex items-center gap-3 text-sm font-medium text-dark">
+              <span>London Internship Program</span>
+              <BiChevronDown className="w-4 h-4" />
+            </span>
+          </div>
         </div>
       </div>
     </MainLayout>
