99ff40abf51bf189084cff027e4dce766eb7570f
gift56 <efeasiughu@gmail.com>
2023-09-27 13:29:44 -0400

    working on the main page

--- src/components/ProgramComponents/ProgramHeader.tsx
+++ src/components/ProgramComponents/ProgramHeader.tsx
@@ -48,15 +48,15 @@ const ProgramHeader = () => {
         </div>
         <div
           ref={selectRef}
-          className={`w-full absolute bg-white flex flex-col items-start justify-start rounded-bl-2xl rounded-br-2xl top-12 shadow-selectShad z-10`}
+          className={`${
+            openSelect ? "scale-100 opacity-100" : "scale-0 opacity-0"
+          } w-full absolute bg-white flex flex-col items-start justify-start rounded-bl-2xl rounded-br-2xl top-12 shadow-selectShad z-10 transition-all duration-300`}
         >
           {selectOptions.map((item) => (
             <div
               onClick={() => openSelection(item.text)}
               key={item.text}
-              className={`${
-                openSelect ? "scale-100 opacity-100" : "scale-0 opacity-0"
-              } w-full border-b items-center last:rounded-bl-2xl flex last:rounded-br-2xl last:border-none justify-between gap-4 shadow-selectShad hover:bg-hovergray hover:text-primary cursor-pointer transition-all duration-300 text-dark py-4 px-4 ${
+              className={`w-full border-b items-center last:rounded-bl-2xl flex last:rounded-br-2xl last:border-none justify-between gap-4 shadow-selectShad hover:bg-hovergray hover:text-primary cursor-pointer transition-all duration-300 text-dark py-4 px-4 ${
                 selectedItem === item.text
                   ? "bg-hovergray text-primary font-medium"
                   : " font-normal"
