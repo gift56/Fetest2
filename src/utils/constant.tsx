fd6bc2a4f9686a7d8d7ea67c954447ad05ac7f82
gift56 <efeasiughu@gmail.com>
2023-09-27 08:12:04 -0400

    working on layout

--- src/utils/constant.tsx
+++ src/utils/constant.tsx
@@ -1,13 +1,26 @@
 import { GrHomeRounded } from "react-icons/gr";
-import { PiUsersThreeBold } from "react-icons/pi";
+import { RxShare1 } from "react-icons/rx";
+import { PiUsersThreeBold, PiCalendarCheckLight } from "react-icons/pi";
 
 export const sidebarLink = [
   {
     text: "Home",
     icon: GrHomeRounded,
+    to: "/",
   },
   {
     text: "User",
     icon: PiUsersThreeBold,
+    to: "/",
+  },
+  {
+    text: "Review Date",
+    icon: PiCalendarCheckLight,
+    to: "/",
+  },
+  {
+    text: "Application Review",
+    icon: RxShare1,
+    to: "/application",
   },
 ];
