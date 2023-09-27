f87c3717b3a93fc2f1404370051d1b9033bfab8f
gift56 <efeasiughu@gmail.com>
2023-09-27 14:05:11 -0400

    working on programView page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -6,8 +6,9 @@ const ProgramViewPage = () => {
     <MainLayout title="FE OverView Task Test - Program Application View">
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
         <ProgramHeader />
-        <div className="w-full flex items-start justify-start gap-6">
-          
+        <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
+          <div className="w-full lg:flex-[1]"></div>
+          <div className="w-full lg:flex-[2]"></div>
         </div>
       </div>
     </MainLayout>
