c744b497da9850dc3d365419c7ffdf7e35c3f1ab
gift56 <efeasiughu@gmail.com>
2023-09-27 08:53:22 -0400

    done working on constants files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import ProgramViewPage from "./pages/ProgramViewPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<ProgramViewPage />} />
+    </Routes>
+  );
 };
 
 export default App;
