814c225defa0111f4007130fde0d7288d9ede648
gift56 <efeasiughu@gmail.com>
2023-09-27 16:40:41 -0400

    working on opportunity

--- src/components/OpportunityComponents/RecommendedChart.tsx
+++ src/components/OpportunityComponents/RecommendedChart.tsx
@@ -33,11 +33,11 @@ export const options = {
   },
 };
 
+const labels = ["January", "Ferbuary", "March", "April", "May", "June", "July"];
+
 const RecommendedChart = () => {
   return (
-    <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8 hidden lg:flex flex-col shadow-cardShad">
-
-    </div>
+    <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8 hidden lg:flex flex-col shadow-cardShad"></div>
   );
 };
 
