57301b2fde98ecb282ce95f39fb5ea9f37636665
gift56 <efeasiughu@gmail.com>
2023-09-27 14:07:42 -0400

    working on programView page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -7,8 +7,8 @@ const ProgramViewPage = () => {
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
         <ProgramHeader />
         <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
-          <div className="w-full lg:flex-[1]"></div>
-          <div className="w-full lg:flex-[2]"></div>
+          <div className="w-full lg:flex-[1] bg-primary p-3"></div>
+          <div className="w-full lg:flex-[2.5] bg-red-500 p-3"></div>
         </div>
       </div>
     </MainLayout>
