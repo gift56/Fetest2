50f04daeb0696b6fef9425abea308914703c9bdc
gift56 <efeasiughu@gmail.com>
2023-09-27 14:22:47 -0400

    working on programView page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -11,6 +11,8 @@ const ProgramViewPage = () => {
     e.preventDefault();
   };
 
+  console.log(searchTerm);
+
   return (
     <MainLayout title="FE OverView Task Test - Program Application View">
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
@@ -27,7 +29,12 @@ const ProgramViewPage = () => {
               >
                 <GoSearch className="w-5 h-5" />
               </button>
-              <input type="text" className="w-full h-full text-sm font-normal text-inputgray outline-none border-none" placeholder="Serach by name, edu, exp or #tag" />
+              <input
+                type="text"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-full h-full text-sm font-normal text-inputgray outline-none border-none"
+                placeholder="Serach by name, edu, exp or #tag"
+              />
               <button
                 type="button"
                 className="pr-3 h-full flex items-center justify-center text-inputgray"
