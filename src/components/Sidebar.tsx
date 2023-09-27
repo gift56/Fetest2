d55345747875186c0673d6ed18cb6eb09363df6e
gift56 <efeasiughu@gmail.com>
2023-09-27 11:49:16 -0400

    working on route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,10 +4,10 @@ const Sidebar = () => {
   const [collapse, setCollapse] = useState(false);
   return (
     <div
-      className={`w-[72px] transition-all duration-300 h-screen px-4 shadow-sideShad sticky top-0 flex flex-col items-center justify-between overflow-x-hidden overflow-y-auto`}
+      className={`w-[72px] transition-all duration-300 h-screen px-4 py-5 shadow-sideShad sticky top-0 flex flex-col items-center justify-between overflow-x-hidden overflow-y-auto`}
     >
       <ul className="w-full flex flex-col items-center justify-center gap-10">
-        
+        <div className="w-10 h-10 bg-hashgray rounded-full"></div>
       </ul>
     </div>
   );
