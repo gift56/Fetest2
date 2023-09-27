2afdaea75e78353c56f4b92569e13e9ec37a875d
gift56 <efeasiughu@gmail.com>
2023-09-27 14:37:47 -0400

    working on programView page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -45,7 +45,7 @@ const ProgramViewPage = () => {
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
         <ProgramHeader />
         <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
-          <div className="w-full lg:flex-[1] flex flex-col gap-5 items-start justify-start sticky top-6">
+          <div className="w-full lg:flex-[1] flex flex-col gap-6 items-start justify-start sticky top-6">
             <form
               onSubmit={handleSubmit}
               className="w-full h-12 bg-white border border-[#F3F3F3] rounded-lg flex items-center justify-between"
@@ -71,7 +71,7 @@ const ProgramViewPage = () => {
             </form>
             <div className="w-full flex flex-col items-start justify-start bg-white rounded-lg overflow-hidden">
               <div className="py-4 flex items-center justify-between gap-4 w-full">
-                
+
               </div>
             </div>
           </div>
