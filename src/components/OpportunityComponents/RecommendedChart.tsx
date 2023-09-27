01843c16c985fe6b35426ccf40b0317af16f6302
gift56 <efeasiughu@gmail.com>
2023-09-27 16:37:39 -0400

    working on opportunity

--- src/components/OpportunityComponents/RecommendedChart.tsx
+++ src/components/OpportunityComponents/RecommendedChart.tsx
@@ -1,5 +1,9 @@
 const RecommendedChart = () => {
-  return <div>RecommendedChart</div>;
+  return (
+    <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8 hidden lg:flex flex-col shadow-cardShad">
+
+    </div>
+  );
 };
 
 export default RecommendedChart;
