8a8a7a3276aa975326f307abc708f61b6ae1425e
gift56 <efeasiughu@gmail.com>
2023-09-27 14:12:28 -0400

    working on programView page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -1,14 +1,19 @@
+import { FormEvent } from "react";
 import { ProgramHeader } from "../components";
 import MainLayout from "../layout/MainLayout";
 
 const ProgramViewPage = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <MainLayout title="FE OverView Task Test - Program Application View">
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
         <ProgramHeader />
         <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
           <div className="w-full lg:flex-[1] flex flex-col gap-5 items-start justify-start">
-
+            <form onSubmit={handleSubmit}></form>
           </div>
           <div className="w-full lg:flex-[2.5]"></div>
         </div>
