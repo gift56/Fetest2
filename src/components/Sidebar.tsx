9f579ea49b576903ff128f518d6c6e1a61d1da25
gift56 <efeasiughu@gmail.com>
2023-09-27 11:46:41 -0400

    working on route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,13 @@
-const Sidebar = () => {
-  return (
-    <div className="w-[72px] transition-all duration-300 h-screen px-4 shadow-sideShad sticky top-0 flex flex-col items-center justify-between overflow-x-hidden overflow-y-auto">
-      <ul className="w-full flex flex-col items-center justify-center gap-10">
+import { useState } from "react";
 
+const Sidebar = () => {
+  const [collapse, setCollapse] = useState(false);
+  return (
+    <div
+      className={`w-[72px] transition-all duration-300 h-screen px-4 shadow-sideShad sticky top-0 flex flex-col items-center justify-between overflow-x-hidden overflow-y-auto`}
+    >
+      <ul className="w-full flex flex-col items-center justify-center gap-10">
+        
       </ul>
     </div>
   );
