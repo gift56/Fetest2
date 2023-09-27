4fec50407bb3ee884306a2ffce99b4846da7dda9
gift56 <efeasiughu@gmail.com>
2023-09-27 08:56:31 -0400

    working on route

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -1,5 +1,11 @@
+import MainLayout from "../layout/MainLayout";
+
 const ProgramViewPage = () => {
-  return <div>ProgramViewPage</div>;
+  return (
+    <MainLayout title="FE OverView Task Test - Program Application View">
+      ProgramViewPage
+    </MainLayout>
+  );
 };
 
 export default ProgramViewPage;
