6b72ab0458bee9d120ae9cc84d5bcf2d6100b4e4
gift56 <efeasiughu@gmail.com>
2023-09-27 16:46:16 -0400

    working on opportunity

--- src/components/OpportunityComponents/RecommendedChart.tsx
+++ src/components/OpportunityComponents/RecommendedChart.tsx
@@ -39,6 +39,26 @@ const qualifiedData = [250, 750, 600, 400, 200, 300, 660];
 
 const disqualifiedData = [250, 350, 200, 350, 200, 300, 560];
 
+export const data: any = {
+  labels,
+  datasets: [
+    {
+      label: "Inflow",
+      data: qualifiedData,
+      borderColor: "rgb(251, 227, 142)",
+      backgroundColor: "rgba(251, 227, 142, 0.70)",
+      cubicInterpolationMode: "monotone",
+    },
+    {
+      label: "Outflow",
+      data: disqualifiedData,
+      borderColor: "rgb(177, 165, 255)",
+      backgroundColor: "rgba(154, 137, 255, 0.70)",
+      cubicInterpolationMode: "monotone",
+    },
+  ],
+};
+
 const RecommendedChart = () => {
   return (
     <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8 hidden lg:flex flex-col shadow-cardShad"></div>
