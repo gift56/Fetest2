cf1f4a9934d6ef59b2483379c4d0a293d60016ff
gift56 <efeasiughu@gmail.com>
2023-09-27 16:38:33 -0400

    working on opportunity

--- src/components/OpportunityComponents/RecommendedChart.tsx
+++ src/components/OpportunityComponents/RecommendedChart.tsx
@@ -1,3 +1,25 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const RecommendedChart = () => {
   return (
     <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8 hidden lg:flex flex-col shadow-cardShad">
