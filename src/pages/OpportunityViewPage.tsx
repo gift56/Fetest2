8887c54db7a13d09803a37f194dbd5ccdd70623e
gift56 <efeasiughu@gmail.com>
2023-09-27 15:52:56 -0400

    working on opportunity

--- src/pages/OpportunityViewPage.tsx
+++ src/pages/OpportunityViewPage.tsx
@@ -5,7 +5,7 @@ const OpportunityViewPage = () => {
   return (
     <MainLayout title="FE OverView Task Test - Opportunity Application View">
       <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
-        <div className="w-full items-center justify-between gap-5">
+        <div className="w-full flex items-center justify-between gap-5">
           <h2 className="text-base font-semibold text-primary md:text-xl">
             Opportunity Overview
           </h2>
