f07571a0ca39d619e77cf1a01cf05b993234560e
gift56 <efeasiughu@gmail.com>
2023-09-27 11:44:32 -0400

    working on route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <div className="w-[72px] transition-all duration-300 h-screen px-4 shadow-sideShad sticky top-0 flex flex-col items-center justify-between">
+    <div className="w-[72px] transition-all duration-300 h-screen px-4 shadow-sideShad sticky top-0 flex flex-col items-center justify-between overflow-x-hidden overflow-y-auto">
       <ul className="w-full flex flex-col items-center justify-center gap-10">
 
       </ul>
