56887b081be72c465bae545273378be488b39a1a
gift56 <efeasiughu@gmail.com>
2023-09-27 15:48:46 -0400

    working on opportunity

--- src/pages/OpportunityViewPage.tsx
+++ src/pages/OpportunityViewPage.tsx
@@ -1,7 +1,13 @@
 import MainLayout from "../layout/MainLayout";
 
 const OpportunityViewPage = () => {
-  return <MainLayout title="FE OverView Task Test - Opportunity Application View"></MainLayout>
+  return (
+    <MainLayout title="FE OverView Task Test - Opportunity Application View">
+      <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
+        <div className="w-full items-center justify-between gap-5"></div>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default OpportunityViewPage;
