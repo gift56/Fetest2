537060815a97b6fcbcbf3403e074196d8de1d476
gift56 <efeasiughu@gmail.com>
2023-09-27 12:33:46 -0400

    working on the main page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -1,9 +1,12 @@
+import { ProgramHeader } from "../components";
 import MainLayout from "../layout/MainLayout";
 
 const ProgramViewPage = () => {
   return (
     <MainLayout title="FE OverView Task Test - Program Application View">
-      <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden"></div>
+      <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
+        <ProgramHeader />
+      </div>
     </MainLayout>
   );
 };
