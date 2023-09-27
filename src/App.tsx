ff6fabad4523db29c234a5a684fce81ea439831e
gift56 <efeasiughu@gmail.com>
2023-09-27 08:54:37 -0400

    working on route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import ProgramViewPage from "./pages/ProgramViewPage";
+import ApplicationViewPage from "./pages/ApplicationViewPage";
+import OpportunityViewPage from "./pages/OpportunityViewPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<ProgramViewPage />} />
+      <Route path="/application" element={<ApplicationViewPage />} />
+      <Route path="/opportunity" element={<OpportunityViewPage />} />
     </Routes>
   );
 };
