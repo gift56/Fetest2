7bc4dc64a25c13f799ea6bb22bf89bd8fa687586
gift56 <efeasiughu@gmail.com>
2023-09-27 14:17:58 -0400

    working on programView page

--- src/pages/ProgramViewPage.tsx
+++ src/pages/ProgramViewPage.tsx
@@ -21,7 +21,7 @@ const ProgramViewPage = () => {
               className="w-full h-12 bg-white border border-[#F3F3F3] rounded-lg flex items-center justify-between gap-5"
             >
               <button type="submit" className="px-3 h-full flex items-center justify-center text-inputgray">
-                <GoSearch className="w-4 h-4" />
+                <GoSearch className="w-5 h-5" />
               </button>
             </form>
           </div>
