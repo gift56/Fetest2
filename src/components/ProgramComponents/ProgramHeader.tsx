e81d9a85b6813040def36b7ee93ea9b046bf1666
gift56 <efeasiughu@gmail.com>
2023-09-27 12:37:41 -0400

    working on the main page

--- src/components/ProgramComponents/ProgramHeader.tsx
+++ src/components/ProgramComponents/ProgramHeader.tsx
@@ -4,11 +4,11 @@ const ProgramHeader = () => {
       aria-label="programheading"
       className="w-full flex items-start justify-between gap-8"
     >
-      <div className="flex items-start justify-start">
+      <div className="flex flex-col items-start justify-start">
         <h2 className="text-base font-semibold text-primary md:text-xl">
           London Internship Program
         </h2>
-        <span className="">London</span>
+        <span className="text-sm text-dark font-normal">London</span>
       </div>
     </nav>
   );
