79e7ba1f8908ad23e98638be3025bddf528d5a74
gift56 <efeasiughu@gmail.com>
2023-09-27 23:27:12 -0400

    working on opportunity detail overview

--- src/components/OpportunityComponents/DetailedOverview.tsx
+++ src/components/OpportunityComponents/DetailedOverview.tsx
@@ -1,7 +1,13 @@
+import { useEffect, useRef, useState } from "react";
+
 const DetailedOverview = () => {
-  return <div className="flex flex-col items-start justify-start w-full gap-6">
-    
-  </div>;
+  return (
+    <div className="flex flex-col items-start justify-start w-full gap-6">
+      <div className="w-full flex items-center justify-between gap-6">
+
+      </div>
+    </div>
+  );
 };
 
 export default DetailedOverview;
