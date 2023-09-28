2c5244bcc27512de864532f116ecfc57b8c1cd57
gift56 <efeasiughu@gmail.com>
2023-09-27 23:24:09 -0400

    working on opportunity page

--- src/components/OpportunityComponents/DetailedOverview.tsx
+++ src/components/OpportunityComponents/DetailedOverview.tsx
@@ -1,5 +1,7 @@
 const DetailedOverview = () => {
-  return <div>DetailedOverview</div>;
+  return <div className="flex flex-col items-start justify-start w-full gap-6">
+    
+  </div>;
 };
 
 export default DetailedOverview;
