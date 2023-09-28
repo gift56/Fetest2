09f6a3edb39eafed65dba0906acbd0a6389cbd59
gift56 <efeasiughu@gmail.com>
2023-09-28 09:50:51 -0400

    working on opportunity detail overview

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         selectShad: "0px 4px 25px 0px rgba(29, 78, 216, 0.05)",
         iconShad: "0px 4px 25px 0px rgba(141, 141, 141, 0.05)",
         cardShad: "2px 4px 40px 2px rgba(205, 223, 255, 0.15)",
+        tooltipShad: "0px 3px 6px -4px rgba(0, 0, 0, 0.12), 0px 6px 16px 0px rgba(0, 0, 0, 0.08), 0px 9px 28px 8px rgba(0, 0, 0, 0.05)",
       },
       screens: {
         laptop: "1390px",
