02fe82099d450dd96562e60fc6981e2951bc8683
gift56 <efeasiughu@gmail.com>
2023-09-29 12:34:33 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/MoreTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/MoreTab.tsx
@@ -1,10 +1,13 @@
 const MoreTab = () => {
+  const otherProgramData = [{}];
   return (
     <div
       id="more"
       className="w-full bg-white rounded-2xl p-5 flex flex-col gap-6 items-start justify-start"
     >
-      <h2 className="text-sm font-semibold text-dark">Additional Questions</h2>
+      <h2 className="text-sm font-semibold text-dark">
+        Enrolled Other Programs
+      </h2>
     </div>
   );
 };
