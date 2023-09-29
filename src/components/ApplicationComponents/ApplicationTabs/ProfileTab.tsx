057b780a012779c6e69ecf963243c233f376937a
gift56 <efeasiughu@gmail.com>
2023-09-29 10:21:53 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/ProfileTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/ProfileTab.tsx
@@ -1,5 +1,13 @@
 const ProfileTab = () => {
-  return <div id="profile">ProfileTab</div>;
+  return (
+    <div
+      id="profile"
+      className="w-full bg-white rounded-2xl p-5 flex flex-col items-start justify-start"
+    >
+      <h2 className="text-sm font-semibold text-dark">Personal Information</h2>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6"></div>
+    </div>
+  );
 };
 
 export default ProfileTab;
