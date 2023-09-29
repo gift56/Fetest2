43281ea112412c150d4064a1072e47db918ada8c
gift56 <efeasiughu@gmail.com>
2023-09-29 08:17:08 -0400

    working on the application viewpage

--- src/pages/ApplicationViewPage.tsx
+++ src/pages/ApplicationViewPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import MainLayout from "../layout/MainLayout";
-import { candidateData, possibleFilter } from "../utils/constant";
+import { candidateData, possibleFilter, selectOptions } from "../utils/constant";
 import { BiChevronDown } from "react-icons/bi";
 import { GoSearch } from "react-icons/go";
 
@@ -70,8 +70,49 @@ const ApplicationViewPage = () => {
 
   return (
     <MainLayout title="FE Task Test - Application Overview">
-      <div className="w-full h-screen flex flex-col lg:flex-row items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
-        <div className="w-full lg:flex-[1] flex flex-col gap-6 items-start justify-start lg:sticky lg:top-6"></div>
+      <div className="w-full h-screen flex flex-col lg:flex-row items-start justify-start gap-6 py-4 px-6 overflow-y-auto overflow-x-hidden">
+        <div className="w-full lg:flex-[1] flex flex-col gap-3 items-start justify-start lg:sticky lg:top-6">
+          <div className="flex flex-col items-center justify-start w-[321px] h-12 gap-3 bg-white shadow-selectShad rounded-lg relative">
+            <div
+              onClick={() => setOpenSelect(!openSelect)}
+              className="w-full flex items-center justify-between cursor-pointer h-full px-4 text-[#384A69]"
+            >
+              <span className="text-base font-medium">{selectedItem}</span>
+              <BiChevronDown className="w-6 h-5 border-l-2" />
+            </div>
+            <div
+              ref={selectRef}
+              className={`${
+                openSelect
+                  ? "scale-x-100 opacity-100 top-12"
+                  : "scale-x-0 opacity-0 top-0"
+              } w-full absolute bg-white flex flex-col items-start justify-start rounded-bl-2xl rounded-br-2xl shadow-selectShad z-10 transition-all duration-300`}
+            >
+              {selectOptions.map((item) => (
+                <div
+                  onClick={() => openSelection(item.text)}
+                  key={item.text}
+                  className={`w-full border-b items-center last:rounded-bl-2xl flex last:rounded-br-2xl last:border-none justify-between gap-4 shadow-selectShad hover:bg-hovergray hover:text-primary cursor-pointer transition-all duration-300 text-dark py-4 px-4 ${
+                    selectedItem === item.text
+                      ? "bg-hovergray text-primary font-medium"
+                      : " font-normal"
+                  }`}
+                >
+                  <span className="text-base">{item.text}</span>
+                  <span
+                    className={`text-sm w-fit py-1 px-2 rounded-2xl ${
+                      selectedItem === item.text
+                        ? "bg-[#D1DDFF] text-primary"
+                        : " bg-[#F8F8F8] text-[#444444]"
+                    }`}
+                  >
+                    {item.totalNumber}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
         <div className="w-full lg:flex-[2.5] flex flex-col items-start justify-start h-screen overflow-y-auto overflow-x-hidden"></div>
       </div>
     </MainLayout>
