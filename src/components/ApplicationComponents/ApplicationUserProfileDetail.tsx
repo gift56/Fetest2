95c46c66335f1e9149b722eb3668df04155da1fb
gift56 <efeasiughu@gmail.com>
2023-09-29 09:03:02 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
+++ src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
@@ -1,13 +1,14 @@
 const ApplicationUserProfileDetail = () => {
   return (
     <div className="w-full bg-white flex items-start justify-between gap-4 flex-col lg:flex-row p-4 rounded-2xl">
-      <div>
+      <div className="flex-shrink">
         <img
           src="/interviewImage.png"
           alt="application_image"
           className="w-[88px] h-[88px] object-contain rounded-full"
         />
       </div>
+      <div className="flex-grow"></div>
     </div>
   );
 };
