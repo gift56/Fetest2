69f4a43df10f20681f778f31cc504c9804b2bb8b
gift56 <efeasiughu@gmail.com>
2023-09-29 08:58:40 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
+++ src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
@@ -1,6 +1,6 @@
 const ApplicationUserProfileDetail = () => {
   return (
-    <div className="w-full bg-white flex items-start justify-between gap-4 flex-col lg:flex-row">
+    <div className="w-full bg-white flex items-start justify-between gap-4 flex-col lg:flex-row p-4 rounded-2xl">
       
     </div>
   );
