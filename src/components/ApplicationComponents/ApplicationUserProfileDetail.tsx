66d607fd04098bbcfb43a21507d553767da5b633
gift56 <efeasiughu@gmail.com>
2023-09-29 09:08:21 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
+++ src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
@@ -1,4 +1,7 @@
 const ApplicationUserProfileDetail = () => {
+  const tag = ["#top_candidate", "#top_candidate"];
+  const experience = ["New York", "Marketing", "London"];
+
   return (
     <div className="w-full bg-white flex items-start justify-between gap-4 flex-col lg:flex-row p-4 rounded-2xl">
       <div className="flex-shrink">
@@ -8,7 +11,22 @@ const ApplicationUserProfileDetail = () => {
           className="w-[88px] h-[88px] object-contain rounded-full"
         />
       </div>
-      <div className="flex-grow"></div>
+      <div className="flex-grow flex flex-col items-start justify-start gap-4">
+        <div className="w-full flex lg:items-center justify-between gap-5 flex-col lg:flex-row items-start">
+          <div className="flex flex-col items-start justify-start gap-3">
+            <h2 className="flex items-center gap-4 text-sm font-semibold text-black">
+              <span>Aaliyah Sanderson</span>
+              <span className="text-xs font-medium text-[#037092] flex items-center justify-start gap-4">
+                {experience.map((item, i) => (
+                  <span key={i} className="bg-[#F3FAFC] py-2 px-4 rounded-xl">
+                    {item}
+                  </span>
+                ))}
+              </span>
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
