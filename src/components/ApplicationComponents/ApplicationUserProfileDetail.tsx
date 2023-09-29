db1ffe14b37deae7e91482f77bcaf7f7c5b1fe97
gift56 <efeasiughu@gmail.com>
2023-09-29 09:31:58 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
+++ src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
@@ -1,5 +1,8 @@
+import { IoMdClose } from "react-icons/io";
+import { RxPlusCircled } from "react-icons/rx";
+
 const ApplicationUserProfileDetail = () => {
-  const tag = ["#top_candidate", "#top_candidate"];
+  const tag = ["#top_candidate", "#marketing_june"];
   const experience = ["New York", "Marketing", "London"];
 
   return (
@@ -34,8 +37,32 @@ const ApplicationUserProfileDetail = () => {
           </div>
           <div className="w-fit py-2 px-4 flex items-center bg-white shadow-selectShad gap-8 rounded-full absolute right-0">
             <h3 className="text-sm font-medium text-dark">Overall Score</h3>
-            <span className="w-8 h-8 flex items-center justify-center rounded-full bg-[#F3FAFC] text-sm text-[#6B6B6B] font-medium">7</span>
+            <span className="w-8 h-8 flex items-center justify-center rounded-full bg-[#F3FAFC] text-sm text-[#6B6B6B] font-medium">
+              7
+            </span>
           </div>
+        </div>
+        <div className="w-full flex lg:items-center justify-between gap-5 flex-col lg:flex-row items-start relative">
+          <span className="text-xs font-medium text-[#808EA3] flex items-center justify-start gap-4">
+            {tag.map((item, i) => (
+              <span
+                key={i}
+                className="bg-[#F5F8FF] py-2 px-4 rounded-full flex items-center jus gap-3"
+              >
+                <span>{item}</span>
+                <span className="cursor-pointer">
+                  <IoMdClose />
+                </span>
+              </span>
+            ))}
+            <span className="flex items-center justify-start gap-2 cursor-pointer text-[#9AA6AC]">
+              <RxPlusCircled className="w-4 h-4" />
+              <span>Add tag</span>
+            </span>
+          </span>
+          <span className="text-[#A8A8A8] text-xs font-light">
+            Applied on 12 June 2023
+          </span>
         </div>
       </div>
     </div>
