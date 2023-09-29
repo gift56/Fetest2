08ed81957cf9eab4794fc6686b88617254a523ff
gift56 <efeasiughu@gmail.com>
2023-09-29 08:29:28 -0400

    working on the application viewpage

--- src/pages/ApplicationViewPage.tsx
+++ src/pages/ApplicationViewPage.tsx
@@ -1,6 +1,10 @@
 import { useState, useRef, useEffect } from "react";
 import MainLayout from "../layout/MainLayout";
-import { candidateData, possibleFilter, selectOptions } from "../utils/constant";
+import {
+  candidateData,
+  possibleFilter,
+  selectOptions,
+} from "../utils/constant";
 import { BiChevronDown } from "react-icons/bi";
 import { GoSearch } from "react-icons/go";
 
@@ -18,10 +22,13 @@ const ApplicationViewPage = () => {
   const [data] = useState(candidateData);
   const [search, setSearch] = useState("");
   const [openSelect, setOpenSelect] = useState(false);
-  const [selectedItem, setSelectedItem] = useState("Opportunity Browsing");
+  const [selectedItem, setSelectedItem] = useState({
+    text: "Opportunity Browsing",
+    total: "2325",
+  });
   const selectRef = useRef<HTMLDivElement>(null);
 
-  const openSelection = (item: string) => {
+  const openSelection = (item: any) => {
     setSelectedItem(item);
     setOpenSelect(false);
   };
@@ -72,13 +79,23 @@ const ApplicationViewPage = () => {
     <MainLayout title="FE Task Test - Application Overview">
       <div className="w-full h-screen flex flex-col lg:flex-row items-start justify-start gap-6 py-4 px-6 overflow-y-auto overflow-x-hidden">
         <div className="w-full lg:flex-[1] flex flex-col gap-3 items-start justify-start lg:sticky lg:top-6">
-          <div className="flex flex-col items-center justify-start w-[321px] h-12 gap-3 bg-white shadow-selectShad rounded-lg relative">
+          <div className="flex flex-col items-center justify-start w-full h-12 gap-3 bg-white shadow-selectShad rounded-lg relative">
             <div
               onClick={() => setOpenSelect(!openSelect)}
               className="w-full flex items-center justify-between cursor-pointer h-full px-4 text-[#384A69]"
             >
-              <span className="text-base font-medium">{selectedItem}</span>
-              <BiChevronDown className="w-6 h-5 border-l-2" />
+              <span className="pr-3">
+                <span className="text-sm font-semibold w-8 h-8 flex items-center justify-center bg-[#F4F7FB] uppercase rounded-full">
+                  SIP
+                </span>
+              </span>
+              <span className="text-base font-medium flex items-center justify-between w-full px-2 border-x-2">
+                <span>{selectedItem.text}</span>
+                <span>{selectedItem.total}</span>
+              </span>
+              <span>
+                <BiChevronDown className="w-6 h-6" />
+              </span>
             </div>
             <div
               ref={selectRef}
@@ -93,7 +110,7 @@ const ApplicationViewPage = () => {
                   onClick={() => openSelection(item.text)}
                   key={item.text}
                   className={`w-full border-b items-center last:rounded-bl-2xl flex last:rounded-br-2xl last:border-none justify-between gap-4 shadow-selectShad hover:bg-hovergray hover:text-primary cursor-pointer transition-all duration-300 text-dark py-4 px-4 ${
-                    selectedItem === item.text
+                    selectedItem.text === item.text
                       ? "bg-hovergray text-primary font-medium"
                       : " font-normal"
                   }`}
@@ -101,7 +118,7 @@ const ApplicationViewPage = () => {
                   <span className="text-base">{item.text}</span>
                   <span
                     className={`text-sm w-fit py-1 px-2 rounded-2xl ${
-                      selectedItem === item.text
+                      selectedItem.text === item.text
                         ? "bg-[#D1DDFF] text-primary"
                         : " bg-[#F8F8F8] text-[#444444]"
                     }`}
