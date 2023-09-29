7de92cffe5f04d0089751ae2ea0f83c37469ae6f
gift56 <efeasiughu@gmail.com>
2023-09-29 12:42:26 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/MoreTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/MoreTab.tsx
@@ -29,6 +29,23 @@ const MoreTab = () => {
       <h2 className="text-sm font-semibold text-dark">
         Enrolled Other Programs
       </h2>
+      <div className="w-full flex flex-col items-start justify-start gap-6">
+        {otherProgramData.map((item, index) => (
+          <div className="w-full border-b py-5 flex items-center justify-between gap-5">
+            <div className="w-full  flex items-center justify-start gap-5">
+              <span className="text-dark w-10 h-10 border border-[#ECECEC] flex items-center justify-center rounded-lg">
+                <img src="/paletteIcon.png" alt="icon" />
+              </span>
+              <div className="flex flex-col items-start justify-start gap-2">
+                <h4 className="text-dark font-medium text-sm">{item.title}</h4>
+                <h2 className="text-black font-normal text-sm flex items-center justify-start">
+                  {item.detail}
+                </h2>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
