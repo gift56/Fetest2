f699444a0747ae6c5d641e6e7ab3ad9b3242458e
gift56 <efeasiughu@gmail.com>
2023-09-29 07:04:17 -0400

    moving to the details page itself

--- src/pages/OpportunityDetailsPage.tsx
+++ src/pages/OpportunityDetailsPage.tsx
@@ -1,3 +1,16 @@
+import MainLayout from "../layout/MainLayout";
+import { BiChevronDown } from "react-icons/bi";
+import { LuCalendarDays } from "react-icons/lu";
+import { PiTrendUpLight, PiTrendDown } from "react-icons/pi";
+import {
+  opportunityData,
+  opportunityDisqulified,
+  opportunityQualified,
+  opportunityRecommendData,
+  opportunityStage,
+} from "../utils/constant";
+import { DetailedOverview, RecommendedChart } from "../components";
+
 const OpportunityDetailsPage = () => {
   return <div>OpportunityDetailsPage</div>;
 };
