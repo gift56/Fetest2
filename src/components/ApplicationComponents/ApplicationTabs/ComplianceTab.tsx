f21c69e344f25ed7bcaf8f0fc885cbf27315e62b
gift56 <efeasiughu@gmail.com>
2023-09-29 10:59:35 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/ComplianceTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/ComplianceTab.tsx
@@ -10,10 +10,10 @@ const ComplianceTab = () => {
       <div className="w-full flex items-center justify-between gap-5">
         <h2 className="text-sm font-semibold text-dark">Resume</h2>
         <div className="flex items-center justify-end gap-4">
-          <span className="text-dark w-10 h-10 border border-[#ECECEC] shadow-selectShad flex items-center justify-center rounded-lg">
+          <span className="text-dark cursor-pointer w-10 h-10 border border-[#ECECEC] shadow-selectShad flex items-center justify-center rounded-lg">
             <FiEye size={20} />
           </span>
-          <span className="text-dark w-10 h-10 border border-[#ECECEC] shadow-selectShad flex items-center justify-center rounded-lg">
+          <span className="text-dark cursor-pointer w-10 h-10 border border-[#ECECEC] shadow-selectShad flex items-center justify-center rounded-lg">
             <LiaFileDownloadSolid size={20} />
           </span>
         </div>
