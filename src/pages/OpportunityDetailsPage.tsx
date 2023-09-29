76cb046b955efd4db2815a0160e3606a89a896d6
gift56 <efeasiughu@gmail.com>
2023-09-29 07:22:34 -0400

    done with the details page

--- src/pages/OpportunityDetailsPage.tsx
+++ src/pages/OpportunityDetailsPage.tsx
@@ -87,7 +87,7 @@ const OpportunityDetailsPage = () => {
                   <RecommendedChart />
                 </div>
               </div>
-              <div className="w-full lg:flex-1 flex flex-col items-start justify-end gap-4">
+              <div className="w-full lg:flex-1 flex flex-col items-start justify-end gap-4 pt-12">
                 <div className="w-full bg-white shadow-cardShad border border-[#F5F5F5] p-4 flex flex-col items-start justify-start gap-3 rounded-2xl">
                   <h2 className="text-sm font-medium text-dark">
                     Total Candidate Flow
