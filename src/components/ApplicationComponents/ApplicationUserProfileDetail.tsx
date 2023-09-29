e3e607021738afca29b743afb290ebaab1f648cd
gift56 <efeasiughu@gmail.com>
2023-09-29 09:02:19 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
+++ src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
@@ -1,7 +1,13 @@
 const ApplicationUserProfileDetail = () => {
   return (
     <div className="w-full bg-white flex items-start justify-between gap-4 flex-col lg:flex-row p-4 rounded-2xl">
-      
+      <div>
+        <img
+          src="/interviewImage.png"
+          alt="application_image"
+          className="w-[88px] h-[88px] object-contain rounded-full"
+        />
+      </div>
     </div>
   );
 };
