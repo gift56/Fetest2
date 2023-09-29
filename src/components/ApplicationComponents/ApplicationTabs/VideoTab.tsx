621d34c7d8264c9986f8f760327188e811aa520e
gift56 <efeasiughu@gmail.com>
2023-09-29 11:04:52 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/VideoTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/VideoTab.tsx
@@ -1,5 +1,17 @@
 const VideoTab = () => {
-  return <div>VideoTab</div>;
+  return (
+    <div
+      id="video"
+      className="w-full bg-white rounded-2xl p-5 flex flex-col gap-6 items-start justify-start"
+    >
+      <div className="w-full flex items-center justify-between gap-5">
+        <h2 className="text-sm font-semibold text-dark">
+          Additional Questions
+        </h2>
+        
+      </div>
+    </div>
+  );
 };
 
 export default VideoTab;
