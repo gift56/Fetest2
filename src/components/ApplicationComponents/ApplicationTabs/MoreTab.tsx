9d2d50aeca917935eecc3f1fb96efaa922b6c4c7
gift56 <efeasiughu@gmail.com>
2023-09-29 12:32:43 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/MoreTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/MoreTab.tsx
@@ -1,5 +1,12 @@
 const MoreTab = () => {
-  return <div>MoreTab</div>;
+  return (
+    <div
+      id="more"
+      className="w-full bg-white rounded-2xl p-5 flex flex-col gap-6 items-start justify-start"
+    >
+      <h2 className="text-sm font-semibold text-dark">Additional Questions</h2>
+    </div>
+  );
 };
 
 export default MoreTab;
