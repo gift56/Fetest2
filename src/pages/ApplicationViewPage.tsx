f3fa7cf99650e134b36be9d1b2f5ee69d5ef2679
gift56 <efeasiughu@gmail.com>
2023-09-29 07:54:07 -0400

    working on the application viewpage

--- src/pages/ApplicationViewPage.tsx
+++ src/pages/ApplicationViewPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import MainLayout from "../layout/MainLayout";
-import { candidateData } from "../utils/constant";
+import { candidateData, possibleFilter } from "../utils/constant";
 import { BiChevronDown } from "react-icons/bi";
 import { GoSearch } from "react-icons/go";
 
@@ -15,9 +15,47 @@ interface Data {
 }
 
 const ApplicationViewPage = () => {
+  const [data] = useState(candidateData);
+  const [search, setSearch] = useState("");
+  const [selectedItems, setSelectedItems] = useState<Data[]>([]);
+
+  const handleItemClick = (item: Data) => {
+    const updatedSelectedItems = [...selectedItems, item];
+    setSelectedItems(updatedSelectedItems);
+  };
+
+  const keys: (keyof Data)[] = [
+    "name",
+    "nationality",
+    "education",
+    "tag",
+    "experience",
+  ];
+
+  const searchTerm = (data: Data[]) => {
+    return data.filter((item) =>
+      keys.some((key) => {
+        const value = item[key];
+        if (typeof value === "string") {
+          return value.toLowerCase().includes(search);
+        } else if (
+          Array.isArray(value) &&
+          value.every((v) => typeof v === "string")
+        ) {
+          return value.some((v) => v.toLowerCase().includes(search));
+        }
+        return false;
+      })
+    );
+  };
+
+  const searchedDatas = searchTerm(data);
+
   return (
     <MainLayout title="FE Task Test - Application Overview">
-      ApplicationViewPage
+      <div className="w-full h-screen flex flex-col items-start justify-start gap-6 py-8 px-6 overflow-y-auto overflow-x-hidden">
+        
+      </div>
     </MainLayout>
   );
 };
