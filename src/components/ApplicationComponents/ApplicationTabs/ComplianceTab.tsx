cbe66e18e1fe87fd1109831abdec387189131bd4
gift56 <efeasiughu@gmail.com>
2023-09-29 10:54:56 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/ComplianceTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/ComplianceTab.tsx
@@ -1,5 +1,15 @@
+import { FiEye } from "react-icons/fi";
+import { LiaFileDownloadSolid } from "react-icons/lia";
+
 const ComplianceTab = () => {
-  return <div>ComplianceTab</div>;
+  return (
+    <div
+      id="compliance"
+      className="w-full bg-white rounded-2xl p-5 flex flex-col gap-6 items-start justify-start"
+    >
+      ComplianceTab
+    </div>
+  );
 };
 
 export default ComplianceTab;
