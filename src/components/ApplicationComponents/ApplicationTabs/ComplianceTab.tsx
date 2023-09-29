25effb71a99e09bee0ae8868e0aa012c8b462564
gift56 <efeasiughu@gmail.com>
2023-09-29 10:56:50 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/ComplianceTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/ComplianceTab.tsx
@@ -7,7 +7,14 @@ const ComplianceTab = () => {
       id="compliance"
       className="w-full bg-white rounded-2xl p-5 flex flex-col gap-6 items-start justify-start"
     >
-      ComplianceTab
+      <div className="w-full flex items-center justify-between gap-5">
+        <h2 className="text-sm font-semibold text-dark">Resume</h2>
+        <div className="flex items-center justify-end gap-4">
+          <span className="text-dark w-10 h-10 border border-[#ECECEC] flex items-center justify-center rounded-lg">
+            <FiEye size={20} />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
