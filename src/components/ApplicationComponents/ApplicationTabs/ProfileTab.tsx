8a82494ba1c75c357f44b8220931d02e35dcf2f0
gift56 <efeasiughu@gmail.com>
2023-09-29 10:22:19 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationTabs/ProfileTab.tsx
+++ src/components/ApplicationComponents/ApplicationTabs/ProfileTab.tsx
@@ -1,11 +1,14 @@
 const ProfileTab = () => {
+  const profileData = [{
+    
+  }];
   return (
     <div
       id="profile"
       className="w-full bg-white rounded-2xl p-5 flex flex-col items-start justify-start"
     >
       <h2 className="text-sm font-semibold text-dark">Personal Information</h2>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6"></div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 items-start justify-start"></div>
     </div>
   );
 };
