3d5a003677723f2f0c4bef49b5a9bf274c817aa1
gift56 <efeasiughu@gmail.com>
2023-09-29 09:11:39 -0400

    working on the application viewpage

--- src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
+++ src/components/ApplicationComponents/ApplicationUserProfileDetail.tsx
@@ -25,7 +25,7 @@ const ApplicationUserProfileDetail = () => {
               </span>
             </h2>
             <p className="flex items-center justify-start text-xs font-light text-black">
-              <span>Saudi Arabia</span>
+              <span className="border-r-[2px] border-black pr-2">Saudi Arabia</span>
             </p>
           </div>
         </div>
